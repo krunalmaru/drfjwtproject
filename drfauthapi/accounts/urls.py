1c7cfda3e40b7f7e261ec4b4eeb764c6a4664286
krunal <krunal.maru1106@gmail.com>
2023-01-11 16:56:58 +0530

    customer login

--- drfauthapi/accounts/urls.py
+++ drfauthapi/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from accounts import views
 urlpatterns = [
-    path('register', views.CustomerRegistrationView.as_view(),name='customer'),
+    path('register/', views.CustomerRegistrationView.as_view(),name='customerregister'),
+    path('login/', views.CustomerLoginView.as_view(),name='customerlogin'),
+
 ]
--- drfauthapi/accounts/views.py
+++ drfauthapi/accounts/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializer import CustomerRegistartionSerializer
+from .serializer import CustomerRegistartionSerializer,CustomerLoginSerializer
+from django.contrib.auth import authenticate,login,logout
+from accounts.renderers import CustomerRender
+
 # Create your views here.
 class CustomerRegistrationView(APIView):
-    def post(self, request,formate=None):
+    renderer_classes = [CustomerRender]
+    def post(self, request,format=None):
         serializer = CustomerRegistartionSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response({'msg':'Registration Successful'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class CustomerLoginView(APIView):
+    def post(self, request, format = None):
+        serializer = CustomerLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            cust = authenticate(email=email,password=password)
+            if cust is not None:
+                return Response({'msg':'Login succsessfully'},status=status.HTTP_200_OK)
+            else:
+                return Response({'errors':{'non_fields_errors':['Email or Password is Not Valid']}},status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
