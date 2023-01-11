6e86d01618989eb009c887711c1414cfb6f2e673
krunal <krunal.maru1106@gmail.com>
2023-01-11 17:56:09 +0530

    change password view and serilizer

--- drfauthapi/accounts/urls.py
+++ drfauthapi/accounts/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('register/', views.CustomerRegistrationView.as_view(),name='customerregister'),
     path('login/', views.CustomerLoginView.as_view(),name='customerlogin'),
     path('profile/', views.CustomerProfileView.as_view(), name='Customerprofile'),
+    path('changepassword/', views.CustomerChangePasswordView.as_view(),name='passwordchange'),
 
 ]
--- drfauthapi/accounts/views.py
+++ drfauthapi/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializer import CustomerRegistartionSerializer,CustomerLoginSerializer,CustomerProfileSerializer
+from .serializer import CustomerRegistartionSerializer,CustomerLoginSerializer,CustomerProfileSerializer,CustomerPasswordChangeSerializer
 from django.contrib.auth import authenticate,login,logout
 from accounts.renderers import CustomerRender
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -48,3 +48,12 @@ class CustomerProfileView(APIView):
     def get(self,request,format=None):
         serializer = CustomerProfileSerializer(request.user)
         return Response(serializer.data,status=status.HTTP_200_OK)
+
+class CustomerChangePasswordView(APIView):
+    renderer_classes = [CustomerRender]
+    permission_classes = [IsAuthenticated]
+    def post(self, request,format=None):
+        serializer = CustomerPasswordChangeSerializer(data=request.data,context={'user':request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg':"Password Change Successfully"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
